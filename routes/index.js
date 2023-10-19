01f4748ef90a7ea850da85d1d97a3424facd2363
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2023-10-19 16:41:25 -0400

    edited the routes module to include CORS

--- routes/index.js
+++ routes/index.js
@@ -28,28 +28,96 @@
 
 // // module.exports = router;
 
+// const express = require("express");
+// const mongoose = require("mongoose");
+//  const routes = require("./routes"); 
+// const cors = require("cors");
+
+// const app = express();
+
+// // Set up middleware
+// app.use(express.json());
+// app.use(cors());
+
+// // Connect to MongoDB
+// mongoose.connect("mongodb://localhost:27017/Marketplaces")
+//   .then(() => {
+   
+//     app.use("/api", routes);
+
+   
+//     app.listen(5000, () => {
+//       console.log("Server has started!");
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection error:", err);
+//   });
+
+
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+
+// mongoose
+//   .connect("mongodb://localhost:27017/MarketPlaces", { useNewUrlParser: true })
+//   .then(() => {
+//     const app = express();
+//     app.use(express.json());
+//     app.use("/api", routes);
+
+//     app.listen(5000, () => {
+//       console.log("Server has started!");
+//     });
+//   });
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+// const app = express();
+
+// mongoose
+//   .connect("mongodb://localhost:27017/MarketPlaces", { useNewUrlParser: true })
+//   .then(() => {
+//     app.use(express.json());
+//     app.use("/api", routes);
+
+//     const PORT = process.env.PORT || 5000;
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   });
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const cors = require("cors"); // Import the 'cors' module
-
-const app = express(); // Initialize the Express app
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors()); // Use the 'cors' middleware
+app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/Marketplaces")
+mongoose
+  .connect("mongodb://localhost:27017/MarketPlaces", { useNewUrlParser: true })
   .then(() => {
-    // Set up routes
-    app.use("/api", routes);
+    app.use(express.json());
+    app.use("/api", routes.router); 
 
-    app.listen(5000, () => {
-      console.log("Server has started!");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
-
-
-
